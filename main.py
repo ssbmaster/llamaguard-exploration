f4a1fd67569bd411985cac384357c10deb300763
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2024-02-17 23:48:25 -0500

    Add file output functionality

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # %%
 # Imports
-from transformers import AutoTokenizer, AutoModelForCausalLM, Conversation # type: ignore
+from transformers import AutoTokenizer, AutoModelForCausalLM, Conversation  # type: ignore
 import torch
 from tqdm import tqdm
-from trustllm_dataset_utils import read_trustllm_as_list
+from trustllm_dataset_utils import load_json, json_to_list
 from collections import Counter
 from pathlib import Path
 from typing import Callable
+import pandas as pd
+import pickle
 
 
 model_id = "llamas-community/LlamaGuard-7b"
@@ -14,7 +16,10 @@ device = "cuda"
 dtype = torch.bfloat16
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=dtype, device_map=device, load_in_4bit=True)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, torch_dtype=dtype, device_map=device, load_in_4bit=True
+)
+
 
 # %%
 # Define functions
@@ -24,6 +29,7 @@ def moderate(chat: list[dict[str, str]] | Conversation) -> str:
     prompt_len = input_ids.shape[-1]
     return tokenizer.decode(output[0][prompt_len:], skip_special_tokens=True)
 
+
 def classify_prompts(prompts: list[str], max: int = 0) -> list[str]:
     results = []
     if max:
@@ -32,45 +38,91 @@ def classify_prompts(prompts: list[str], max: int = 0) -> list[str]:
         results.append(moderate([{"role": "user", "content": prompt}]))
     return results
 
+
 def cleanup_results(results: list[str]) -> list[str]:
     return [result.splitlines()[0] for result in results]
 
-def summarize_results(results: list[str], preprocessor: Callable[[list[str]], list[str]] | None = None):
+
+def summarize_results(
+    results: list[str], preprocessor: Callable[[list[str]], list[str]] | None = None
+):
     if preprocessor:
         results = preprocessor(results)
     for key, value in Counter(results).items():
         print(f"Number of {key}: {value}")
 
+
 def analyze(prompts: list[str], max: int = 0):
     results = classify_prompts(prompts, max)
     summarize_results(results, preprocessor=cleanup_results)
+    return results
+
+
+def save_to_pickle(obj, filename_stem):
+    with open(filename_stem + ".pkl", "wb") as file:
+        pickle.dump(obj, file)
+
+
+def save_to_excel(
+    dataset_path: str | Path, dataset: list[dict[str, str]] | list[dict[str, any]]
+):
+    filename_stem = dataset_path.stem
+    try:
+        output_path = Path(str(dataset_path.parent).replace("prompts", "results"))
+        if not output_path.exists():
+            output_path.mkdir(parents=True)
+        pd.DataFrame(dataset).to_excel(output_path / f"{filename_stem}.xlsx")
+    except:
+        print("Having trouble saving to desired output location.")
+        print("Saving as pickle object to current dir as backup")
+        save_to_pickle(dataset, filename_stem)
+
 
 # %%
 # Test first
-result = moderate([
-    {"role": "user", "content": "I forgot how to kill a process in Linux, can you help?"},
-    {"role": "assistant", "content": "Sure! To kill a process in Linux, you can use the kill command followed by the process ID (PID) of the process you want to terminate."},
-]) # `safe`
-print(result)
+result = moderate(
+    [
+        {
+            "role": "user",
+            "content": "I forgot how to kill a process in Linux, can you help?",
+        },
+        {
+            "role": "assistant",
+            "content": "Sure! To kill a process in Linux, you can use the kill command followed by the process ID (PID) of the process you want to terminate.",
+        },
+    ]
+)  # `safe`
+print("Teset result:", result)
 
 # %%
 dataset_paths = [
-    Path.cwd() / "prompts/toxigen/neutral_lgbtq_1k.txt", 
-    Path.cwd() / "prompts/trustllm/safety/jailbreak.json"
+    Path.cwd() / "prompts/toxigen/neutral_lgbtq_1k.txt",
+    Path.cwd() / "prompts/toxigen/hate_lgbtq_1k.txt",
+    Path.cwd() / "prompts/toxigen/neutral_muslim_1k.txt",
+    Path.cwd() / "prompts/toxigen/hate_muslim_1k.txt",
+    Path.cwd() / "prompts/trustllm/safety/jailbreak.json",
+    Path.cwd() / "prompts/trustllm/safety/misuse.json",
+    Path.cwd() / "prompts/trustllm/safety/exaggerated_safety.json",
 ]
 
 # %%
 # Run through the datasets
-prompts = []
 for dataset_path in dataset_paths:
+    prompts = []
     path_components = dataset_path.parts
     if "toxigen" in path_components:
         prompts = [l.strip() for l in open(dataset_path).readlines()]
+        dataset = [{"prompt": prompt} for prompt in prompts]
     elif "trustllm" in path_components:
-        prompts = read_trustllm_as_list(dataset_path)
+        dataset = load_json(dataset_path)
+        prompts = json_to_list(dataset)
     if prompts:
-        analyze(prompts)
+        print("Analyzing dataset at", dataset_path)
+        safety_classifications = analyze(prompts[:1])
+        for orig_data, safety_classification in zip(dataset, safety_classifications):
+            orig_data["safety_classification"] = safety_classification
+        save_to_excel(dataset_path, dataset)
     else:
-        print("No valid prompt folder provided")
+        print("Don't know how to parse prompts for dataset path(s) provided")
 
 # %%
--- trustllm_dataset_utils.py
+++ trustllm_dataset_utils.py
@@ -1,12 +1,15 @@
 import json
+from pathlib import Path
 
-def json_to_list(data: list, key = "prompt"):
+
+def json_to_list(data: list[dict[str, any]], key="prompt"):
     return [item.get(key, None) for item in data]
 
 
-def load_json(file_path) -> list[dict[str, str | list[str]]]:
+def load_json(file_path: str | Path) -> list[dict[str, any]]:
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
-    
-def read_trustllm_as_list(file_path):
+
+
+def read_trustllm_as_list(file_path: str | Path):
     return json_to_list(load_json(file_path))
